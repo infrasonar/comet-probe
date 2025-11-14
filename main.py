6676f653cee47c1ac46441fb0be570b1f4c0de3f
Koos85 <koosjoosten@gmail.com>
2025-11-14 14:06:11 +0100

    libprobe v2

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from libprobe.probe import Probe
-from lib.check.comet import check_comet
+from lib.check.comet import CheckComet
 from lib.version import __version__ as version
 
 
 if __name__ == '__main__':
-    checks = {
-        'comet': check_comet
-    }
+    checks = (
+        CheckComet,
+    )
 
     probe = Probe("comet", version, checks)
 
